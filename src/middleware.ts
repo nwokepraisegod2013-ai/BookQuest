181e6d4d15ff1529642c2ff468e1584dff6e4aa9
Nwoke PraiseGod <nwokepraisegod2013@gmail.com>
2026-06-06 19:41:42 +0100

    fix: resolve Vercel middleware issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,18 +11,18 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/webhooks(.*)",
-  "/api/books/.*/sample",
+  "/api/books/(.*)/sample",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return;
-
-  auth.protect();
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
   matcher: [
-    // Run middleware on all routes except static assets
-    "/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|uploads)).*)",
+    "/(api|trpc)(.*)",
   ],
 };
